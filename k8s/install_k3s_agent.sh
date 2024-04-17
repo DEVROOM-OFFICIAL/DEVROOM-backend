82e92d62005bff2f00c1375cce331f83f1d6e25b
Yanghyeondong <hdyang0686@naver.com>
2024-04-17 12:36:35 +0000

    ✨k8s script 완성 및 readme 수정

--- k8s/install_k3s_agent.sh
+++ k8s/install_k3s_agent.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+: '
+스크립트 권한 설정
+sudo chmod 755 install_k3s_server.sh
+sudo chmod 755 install_k3s_agent.sh
+
+git email 설정
+git config --global user.email "hdyang0686@naver.com"
+git config --global user.name "Yanghyeondong"
+'
+
 read -p "Enter the K3S server address: " myserver
 read -p "Enter the K3S node token: " mynodetoken
 
-curl -sfL https://get.k3s.io | K3S_URL=https://${myserver}:6443 K3S_TOKEN=${mynodetoken} sh -s - --disable=traefik --write-kubeconfig-mode=644 > k3s_install.log
+curl -sfL https://get.k3s.io | K3S_URL=https://${myserver}:6443 K3S_TOKEN=${mynodetoken} sh -s - > k3s_install.log
