0d3acaa54a771747793bb1f9a081ddeb7ab12c35
Yanghyeondong <hdyang0686@naver.com>
2024-04-17 22:24:53 +0900

    🔥 Spring Security 관련 코드 및 파일 삭제

--- backend/src/main/java/com/devlatte/devroom/dto/NormalResponse.java
+++
@@ -1,6 +0,0 @@
-package com.devlatte.devroom.dto;
-
-import org.springframework.http.HttpStatus;
-
-public record NormalResponse(HttpStatus status, String message) {
-}
--- backend/src/main/java/com/devlatte/devroom/entity/MemberRole.java
+++
@@ -1,5 +0,0 @@
-package com.devlatte.devroom.entity;
-
-public enum MemberRole {
-    Student, Professor;
-}
