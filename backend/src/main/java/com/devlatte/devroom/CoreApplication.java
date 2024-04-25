8296541bdb1b703cffaef0888e7140111f099e10
legionary7931 <legionary7931@naver.com>
2024-04-25 15:21:01 +0900

    added lambda logger, changed handler to RequestHandler

--- backend/src/main/java/com/devlatte/devroom/CoreApplication.java
+++ backend/src/main/java/com/devlatte/devroom/CoreApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoreApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CoreApplication.class, args);
     }
-
 }
