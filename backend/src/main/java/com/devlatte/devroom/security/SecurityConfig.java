e0afa729ea39cc3a611018cb4bb43c7a615422ec
Yanghyeondong <hdyang0686@naver.com>
2024-05-01 20:05:05 +0900

    ✨ 그룹 대신 custom attribute 사용

--- backend/src/main/java/com/devlatte/devroom/security/SecurityConfig.java
+++ backend/src/main/java/com/devlatte/devroom/security/SecurityConfig.java
@@ -2,14 +2,24 @@ package com.devlatte.devroom.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+
+import java.util.function.Supplier;
 
 @Slf4j
 @EnableWebSecurity(debug = true)
@@ -24,8 +34,10 @@ public class SecurityConfig {
         return http
                 .authorizeHttpRequests(auth ->{auth
                         .requestMatchers("/", "/error").permitAll()
-                        .requestMatchers("/pod/**", "/service/**").hasAuthority("ROLE_Student")
                         .requestMatchers("/**").hasAuthority("ROLE_Professor")
+                        .requestMatchers("/pod/**", "/service/**").hasAuthority("ROLE_Student")
+                        // 본인의 id의 주소로만 접근이 가능하게 해야할 것 같습니다. 검색해본 결과 아래 같은 느낌인것 같지만, 정확하게 구현이 필요합니다.
+                        // .requestMatchers("/pod/student_id/{id}", "/service/**").access()
                         .anyRequest().authenticated();
                 })
                 .oauth2ResourceServer(oauth2 -> oauth2
@@ -46,3 +58,5 @@ public class SecurityConfig {
     }
 
 }
+
+
