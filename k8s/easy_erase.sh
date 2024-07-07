d650ece6a2c66596942917c897c44279a29983ca
Yanghyeondong <hdyang0686@naver.com>
2024-07-07 20:27:39 +0900

    ✨ easy_erase 스크립트 파일 추가

---
+++ k8s/easy_erase.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+: '
+sudo chmod 755 easy_erase.sh
+'
+
+# 사용자로부터 키워드 입력 받기
+read -p "삭제할 키워드를 입력하세요: " keyword
+
+# 입력된 키워드를 포함하는 서비스 삭제
+kubectl get svc | grep $keyword | awk '{print $1}' | xargs -r kubectl delete svc
+
+# 입력된 키워드를 포함하는 컨피그맵 삭제
+kubectl get cm | grep $keyword | awk '{print $1}' | xargs -r kubectl delete cm
+
+# 입력된 키워드를 포함하는 디플로이먼트 삭제
+kubectl get deploy | grep $keyword | awk '{print $1}' | xargs -r kubectl delete deploy
+
+# 입력된 키워드를 포함하는 파드 삭제
+kubectl get pods | grep $keyword | awk '{print $1}' | xargs -r kubectl delete pod
