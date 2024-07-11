ad3d4e87b679b56ca4c9bc10bebf86d07e503dfe
Yanghyeondong <hdyang0686@naver.com>
2024-07-11 21:36:44 +0900

    ✨ 설치 및 삭제 스크립트 수정

--- k8s/easy_erase.sh
+++ k8s/easy_erase.sh
@@ -8,13 +8,13 @@ chmod +x easy_erase.sh
 read -p "삭제할 키워드를 입력하세요: " keyword
 
 # 입력된 키워드를 포함하는 서비스 삭제
-kubectl get svc | grep $keyword | awk '{print $1}' | xargs -r kubectl delete svc --force --grace-period=0
+kubectl get svc | grep $keyword | awk '{print $1}' | xargs -r kubectl delete svc
 
 # 입력된 키워드를 포함하는 컨피그맵 삭제
-kubectl get cm | grep $keyword | awk '{print $1}' | xargs -r kubectl delete cm --force --grace-period=0
+kubectl get cm | grep $keyword | awk '{print $1}' | xargs -r kubectl delete cm
 
 # 입력된 키워드를 포함하는 디플로이먼트 삭제
-kubectl get deploy | grep $keyword | awk '{print $1}' | xargs -r kubectl delete deploy --force --grace-period=0
+kubectl get deploy | grep $keyword | awk '{print $1}' | xargs -r kubectl delete deploy
 
 # 입력된 키워드를 포함하는 파드 삭제
-kubectl get pods | grep $keyword | awk '{print $1}' | xargs -r kubectl delete pod --force --grace-period=0
+kubectl get pods | grep $keyword | awk '{print $1}' | xargs -r kubectl delete pod
