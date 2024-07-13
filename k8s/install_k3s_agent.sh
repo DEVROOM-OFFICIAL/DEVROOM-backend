18d4f953c5b7d715257a44687890a130c3bc48f8
Yanghyeondong <hdyang0686@naver.com>
2024-07-13 15:26:07 +0900

    ✨ 공유폴더 권한 설정 및 설치 스크립트 수정

--- k8s/install_k3s_agent.sh
+++ k8s/install_k3s_agent.sh
@@ -12,13 +12,12 @@ LINE="===================="
 echo "${LINE} Install nfs... ${LINE}"
 read -p "Enter the nfs address: " mynfs
 sudo apt install -y nfs-common >> install.log 2>&1
-sudo mkdir -p /dev-room >> install.log
-sudo mount ${mynfs} /dev-room >> install.log
-sudo chmod -R 777 /dev-room
-ls -al /dev-room
-
-read -p "Enter the K3S server address: " myserver
-read -p "Enter the K3S node token: " mynodetoken
+sudo mkdir -p /nfs_devroom >> install.log
+sudo mount ${mynfs} /nfs_devroom >> install.log
+sudo chmod -R 777 /nfs_devroom
+ls -al /nfs_devroom
 
 echo "${LINE} Install k3s... ${LINE}"
+read -p "Enter the K3S server address: " myserver
+read -p "Enter the K3S node token: " mynodetoken
 curl -sfL https://get.k3s.io | K3S_URL=https://${myserver}:6443 K3S_TOKEN=${mynodetoken} sh -s - > k3s_install.log
